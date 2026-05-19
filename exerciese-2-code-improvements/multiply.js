16a4fc0d35d4e0f417b42618add37f1978116dc8
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2026-05-19 17:04:38 +0200

    chore: add improvement for multiply.js

---
+++ exerciese-2-code-improvements/multiply.js
@@ -0,0 +1,19 @@
+/*
+
+Function that needs improvements 
+
+
+function mult(x,y){
+  return x*y;
+}
+
+*/
+
+const multiply = (number1, number2) => {
+  return number1 * number2;
+};
+
+console.log(multiply(2, 3));
+console.log(multiply(0, 3));
+console.log(multiply(2, 0));
+console.log(multiply(2, 3));
