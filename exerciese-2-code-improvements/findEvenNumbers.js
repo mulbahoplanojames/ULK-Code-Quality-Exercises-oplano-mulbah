ca1f93dbd84fa7865ce28b01752802b82cef9417
mulbahoplanojames <mulbahjamesoplano@gmail.com>
2026-05-22 14:16:19 +0200

    refactor: improve the code quality of all files with in the exercise-2-code-imporvement

--- exerciese-2-code-improvements/findEvenNumbers.js
+++ exerciese-2-code-improvements/findEvenNumbers.js
@@ -1,7 +1,6 @@
 function findEvenNumbers(numbers) {
-  return numbers.filter((number) => number % 2 === 0)
+  return numbers.filter((number) => number % 2 === 0);
 }
 
 const testNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
 console.log(findEvenNumbers(testNumbers));
--- exerciese-2-code-improvements/maxNumber.js
+++ exerciese-2-code-improvements/maxNumber.js
@@ -1,6 +1,7 @@
 const maxNumber = (...numbers) => {
   return Math.max(...numbers);
 };
+
 console.log(maxNumber(1, 2, 3));
 console.log(maxNumber(10, 5, 2));
 console.log(maxNumber(20, 25, 30));
